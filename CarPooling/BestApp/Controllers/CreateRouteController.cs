f0bbe1914586a91cb6f07a630acd4ba8194b9a3b
anateodoragrigore <anna730punct@yahoo.com>
2014-11-11 15:02:00 +0200

    Butoane respectiv paginile lor (controllers and views)

---
+++ CarPooling/BestApp/Controllers/CreateRouteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BestApp.Controllers
+{
+    public class CreateRouteController : Controller
+    {
+        // GET: CreateRoute
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CarPooling/BestApp/Controllers/SearchRouteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BestApp.Controllers
+{
+    public class SearchRouteController : Controller
+    {
+        // GET: SearchRoute
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
