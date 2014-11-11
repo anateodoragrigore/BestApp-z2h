7177985110192e45032dd3ebad6b18362ad51e89
anateodoragrigore <anna730punct@yahoo.com>
2014-11-11 17:15:58 +0200

    Form in CreateRoute (am facut modelul am adaugat inputurile hidden pt geolocatii)

--- CarPooling/BestApp/Controllers/CreateRouteController.cs
+++ CarPooling/BestApp/Controllers/CreateRouteController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BestApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,8 +11,9 @@ namespace BestApp.Controllers
     {
         // GET: CreateRoute
         public ActionResult Index()
-        {
-            return View();
+        { 
+            CreateRouteModel model = new CreateRouteModel();
+            return View(model);
         }
     }
 }
---
+++ CarPooling/BestApp/Models/CreateRouteModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BestApp.Models
+{
+    public class CreateRouteModel
+    {
+        public string Nume { get; set; }
+        public string Prenume { get; set; }
+        public string Telefon { get; set; }
+        public string Email { get; set; }
+        public int NrLocuri { get; set; }
+        public double LatPlecare { get; set; }
+        public double LongPlecare { get; set; }
+        public double LatSosire { get; set; }
+        public double LongSosire { get; set; }
+    }
+}
