f31df1a1669ed84a7b796b4326bce5b6ad32366a
Dumitru Andreea <andreean.dumitru@yahoo.com>
2014-11-12 10:54:17 +0200

    Added entities, models  for DB

---
+++ CarPooling/BestApp.Models/BestAppContext.cs
@@ -0,0 +1,22 @@
+﻿using BestApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BestApp.Models
+{
+    public class BestAppContext:DbContext
+    {
+        public BestAppContext()
+            : base("BestAppContext")
+        {
+
+        }
+        public virtual DbSet<Track> TrackSet {get;set;}
+        public virtual  DbSet<User> UserSet { get; set; }
+    }
+}
---
+++ CarPooling/BestApp.Models/Migrations/201411120846125_initialMigration.cs
@@ -0,0 +1,41 @@
+namespace BestApp.Models.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initialMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Tracks",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Latitude = c.Double(nullable: false),
+                        Longitude = c.Double(nullable: false),
+                        startHour = c.Time(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        firstName = c.String(),
+                        lastName = c.String(),
+                        phoneNumber = c.String(),
+                        emailAddress = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+            DropTable("dbo.Tracks");
+        }
+    }
+}
