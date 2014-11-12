be80cb205890352ec3b0ce5762180b014af87e8d
Dumitru Andreea <andreean.dumitru@yahoo.com>
2014-11-12 11:37:42 +0200

    Added Entities

---
+++ CarPooling/BestApp.Entities/Track.cs
@@ -0,0 +1,23 @@
+﻿using BestApp.Entities.BaseEntity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Spatial;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace BestApp.Entities
+{
+    public class Track:BaseEntity<int>
+    {
+
+        public TimeSpan StartHour { get; set; }
+        public  DbGeography Start { get; set; }
+        public DbGeography Stop { get; set; }
+        public int CarSeats{ get; set; }
+        public EnumUserType UserType { get; set; }
+
+    }
+}
---
+++ CarPooling/BestApp.Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using BestApp.Entities.BaseEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestApp.Entities
+{
+    public class User: BaseEntity<int>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string  EmailAddress { get; set; }
+    }
+}
