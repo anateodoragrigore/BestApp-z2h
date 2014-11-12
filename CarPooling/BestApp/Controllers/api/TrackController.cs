82eadca178c9d12ee040eb5250e44b9bd085cee6
pirvuflavia <pirvuflaviaalice@gmail.com>
2014-11-12 18:10:44 +0200

    Adaugare Controller care cauta rute pentru SearchRoute

---
+++ CarPooling/BestApp/Controllers/api/TrackController.cs
@@ -0,0 +1,24 @@
+﻿using BestApp.Entities;
+using BestApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BestApp.Controllers
+{
+    public class TrackController : ApiController
+    {
+        public IEnumerable<Track> GetTracks()
+        {
+            using (var model = new BestAppContext())
+            {
+                return model.TrackSet.ToList();
+
+            }
+
+        }
+    }
+}
