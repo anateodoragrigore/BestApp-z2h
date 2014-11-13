44b36f3718fed84fadac576690069157c4c70d10
pirvuflavia <pirvuflaviaalice@gmail.com>
2014-11-13 17:02:23 +0200

    Functie pentru searchRoute button

--- CarPooling/BestApp/Controllers/api/TrackController.cs
+++ CarPooling/BestApp/Controllers/api/TrackController.cs
@@ -2,6 +2,7 @@
 using BestApp.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Spatial;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -11,6 +12,30 @@ namespace BestApp.Controllers
 {
     public class TrackController : ApiController
     {
+
+        private void SaveTrackToDatabase(CreateRouteModel model)
+        {
+            using (var context = new BestAppContext())
+            {
+                var newtrack = new Track()
+                {
+                    CarSeats = model.FreeSeats,
+                    Start = DbGeography.PointFromText(
+                        string.Format("POINT({0} {1})",
+                        model.StartLatitude,
+                        model.StartLongitude)
+                        , 4326),
+                    Stop = DbGeography.PointFromText(
+                        string.Format("POINT({0} {1})",
+                        model.StopLatitude,
+                        model.StopLongitude)
+                        , 4326),
+                    StartHour = model.StartHour
+                };
+
+            }
+        }
+
         public IEnumerable<Track> GetTracks()
         {
             using (var model = new BestAppContext())
