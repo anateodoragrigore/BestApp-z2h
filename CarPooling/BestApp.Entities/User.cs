50c8a5cbfb62bcc86a7c3c978e8e07a76800bd1b
pirvuflavia <pirvuflaviaalice@gmail.com>
2014-11-14 11:58:21 +0200

    Set String Lenght for Name

--- CarPooling/BestApp.Entities/User.cs
+++ CarPooling/BestApp.Entities/User.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace BestApp.Entities
 {
     public class User: BaseEntity<int>
     {
+        [StringLength(250)]
         public string Name { get; set; }
     }
 }
