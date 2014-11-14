1b601a8e31a274197e4c82d8e630bc9bce1d504f
Ciprian93 <tascaciprian@stud.ase.ro>
2014-11-14 18:11:49 +0200

    HeatMap almost working

--- CarPooling/BestApp.Models/Migrations/Configuration.cs
+++ CarPooling/BestApp.Models/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace BestApp.Models.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(BestApp.Models.BestAppContext context)
